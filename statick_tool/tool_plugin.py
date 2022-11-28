f3dc315be423dbc34c9593c77a50ea219f831a1c
Greg Kogut <greg.t.kogut.civ@us.navy.mil>
2022-11-27 20:16:35 -0800

    Wild debugging.

--- statick_tool/tool_plugin.py
+++ statick_tool/tool_plugin.py
@@ -80,6 +80,23 @@ class ToolPlugin(IPlugin):  # type: ignore
 
         print(f"2scanE {level}")
 
+        if files:
+            print("2scanF")
+            total_output = (  # pylint: disable=assignment-from-no-return
+                self.process_files(package, level, files, self.get_user_flags(level))
+            )
+            print("2scanG")
+            if total_output is not None:
+                if self.plugin_context and self.plugin_context.args.output_directory:
+                    print("2scanH")
+                    with open(self.get_name() + ".log", "w", encoding="utf8") as fid:
+                        for output in total_output:
+                            fid.write(output)
+                print("2scanI")
+                return self.parse_output(total_output, package)
+
+            return None
+
         return []
 
     def process_files(
